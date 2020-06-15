82fad9c27ec2b5b61ad848a99b23ae95c80ee44a
maggie97 <maggielee97@icloud.com>
2020-06-15 12:32:26 -0500

    Initial Commit

---
+++ AgendaEscolarUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AgendaEscolarUI
+//
+//  Created by maggie mendez on 15/06/20.
+//  Copyright © 2020 maggie mendez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
